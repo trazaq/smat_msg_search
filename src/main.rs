d5d1ec09f45eb192c395ccae77359caaef30f74b
trazaq <tr59210@gmail.com>
2022-12-18 22:14:41 -0600

    Parsing the server.ini file for sites correctly.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ use askama::Template;
 use axum::response::Html;
 use axum::routing::get;
 use axum::{
-    body::{Body, Bytes},
-    http::{Request, StatusCode},
-    middleware::{self, Next},
+    http::StatusCode,
     response::{IntoResponse, Response},
-    routing::post,
     Router,
 };
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::fs;
 use std::net::SocketAddr;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -28,8 +28,7 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new()
-        .route("/", get(index));
+    let app = Router::new().route("/", get(index));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -64,7 +63,34 @@ where
 }
 
 async fn index() -> impl IntoResponse {
-    let template = HelloTemplate;
-    HtmlTemplate(template)
+    lazy_static! {
+        // have to enable flag '(?m)' to use '^' and '$'
+        static ref RE: Regex =
+            Regex::new(r"(?m)^environs=.*$").expect("Error Compiling Regex Expression");
+    }
+    //let RE = Regex::new(r"(?m)^environs=.*$").expect("Error Compiling Regex Expression");
+    let contents = fs::read_to_string("tests/server.ini");
+    match contents {
+        Ok(contents) => {
+            if let Some(sites) = RE.find(&contents) {
+                let sites: Vec<&str> = sites
+                    .as_str()
+                    .strip_prefix("environs=")
+                    .unwrap()
+                    .split(';')
+                    .into_iter()
+                    .filter_map(|site| site.split('/').last())
+                    .collect();
+                return format!("{:?}", sites);
+            }
+            "No sites".to_string()
+        }
+        Err(e) => {
+            format!("{}", e)
+        }
+    }
+
+    //let template = HelloTemplate;
+    //HtmlTemplate(template)
 }
 //=============================END INDEX==================================================
