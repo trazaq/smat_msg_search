140f0b57ee1c7e5196db9f50f1ae1778b1081122
Tipu Razaq <tipu.razaq@utexas.edu>
2022-12-20 09:15:49 -0600

    formatting

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[tokio::main]
 async fn main() {
     dotenv().ok(); // load .env file variables then use with std::env:: functions
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -19,7 +20,9 @@ async fn main() {
     let app = Router::new().route("/", get(index));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
     tracing::debug!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
