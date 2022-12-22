9dd42fe87a4d32baa695082fc1bc1d44252a15d5
trazaq <tr59210@gmail.com>
2022-12-21 21:44:18 -0600

    Finally able to decrypt a db for reading. Need to polish.

--- src/routes/status/mod.rs
+++ src/routes/status/mod.rs
@@ -2,7 +2,7 @@ use axum::extract::Query;
 use axum::http::header::CONTENT_TYPE;
 use axum::response::IntoResponse;
 use hyper::Response;
-use rusqlite::{Connection, OpenFlags};
+use rusqlite::{Connection, Error, OpenFlags};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -15,16 +15,33 @@ pub struct Site {
 //     row: String,
 // }
 
-pub async fn status(_site: Query<Site>) -> impl IntoResponse {
+pub async fn status(site: Query<Site>) -> impl IntoResponse {
     let conn = Connection::open_with_flags(
-        "tests/iTo_alladt.2022-12-07_00-01-26.smatdb",
+        "tests/fr_verity.2022-12-19_20-29-32.smatdb",
         OpenFlags::SQLITE_OPEN_READ_ONLY,
     )
     .unwrap();
 
-    let mut stmt = conn
-        .prepare(r#"SELECT CAST(MessageContent AS TEXT) FROM smat_msgs ORDER BY TimeIn ASC;"#)
-        .unwrap();
+    let sql = r#"SELECT CAST(MessageContent AS TEXT) FROM smat_msgs ORDER BY TimeIn ASC;"#;
+    let pragma = format!("pragma key = '{}';pragma cipher_compatibility = 3;", site.site);
+    let mut stmt = match conn
+        .prepare(sql)
+    {
+        Ok(stmt) => stmt,
+        Err(e) => match e {
+            Error::SqliteFailure(code, _) => {
+                if code.extended_code == 26 {
+                    conn.execute_batch(&pragma).unwrap();
+                    conn.prepare(sql).unwrap()
+                } else {
+                    panic!("{:?}", code)
+                }
+            }
+            _ => {
+                panic!("{:?}", e)
+            }
+        },
+    };
 
     let mut rows = stmt.query([]).unwrap();
 
@@ -32,7 +49,7 @@ pub async fn status(_site: Query<Site>) -> impl IntoResponse {
     while let Some(row) = rows.next().unwrap() {
         let mut msg: String = row.get(0).unwrap();
         //let mut msg = String::from_utf8_lossy(&msg).to_string();
-        msg = msg.replace("\r", "<br>");
+        msg = msg.replace('\r', "<br>");
         msg.insert_str(0, "<p>");
         msg.push_str("</p>");
         msgs.push(msg);
